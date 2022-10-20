1b534d120fe9b477fd0b1685edb1b9e63b08bc69
panosjapan7 <panosjapan@gmail.com>
2022-10-20 10:53:17 +0200

    Create ProductItem interface

---
+++ frontend/src/interfaces/product-item.ts
@@ -0,0 +1,10 @@
+export default interface IProductItem {
+    _id?: string,
+    name: string,
+    description: string,
+    price: string,
+    manufacturer: string,
+    weight: string,
+    category: string,
+    pictures: string[]
+};
