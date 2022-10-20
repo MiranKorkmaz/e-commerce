0530b112f88278ad30c6012acb8754d646dec403
panosjapan7 <panosjapan@gmail.com>
2022-10-20 15:59:47 +0200

    Create conditional rendering if product is not found

--- frontend/src/pages/ProductItem.tsx
+++ frontend/src/pages/ProductItem.tsx
@@ -13,22 +13,28 @@ const ProductItem = ({allProducts}:HomeProps) => {
     const product = allProducts.find(product => product._id === id);
 
     return (
-        <div className='wrapper--ProductItem'>
-            <div className='container--product-item'></div>
-            <h1>{product?.name} by {product?.manufacturer}</h1>
-            <img className='main-product-image' src={product?.pictures[0]} alt={product?.name} />
-            <div className='container--thumbnails'>
-                {product?.pictures.map((image, index) => (
-                    <img className='thumbnail' src={image} alt={`${product.name} ${index}`}/>
-                ))}
+        <>
+            {product ? (
+                <div className='wrapper--ProductItem'>
+                <div className='container--product-item'></div>
+                <h1>{product?.name} by {product.manufacturer}</h1>
+                <img className='main-product-image' src={product.pictures[0]} alt={product.name} />
+                <div className='container--thumbnails'>
+                    {product?.pictures.map((image, index) => (
+                        <img className='thumbnail' src={image} alt={`${product.name} ${index}`}/>
+                    ))}
+                </div>
+                <div className='container--product-item-details'>
+                    <p><strong>Price:</strong> ${product.price}</p>
+                    <p><strong>Weight:</strong> {product.weight} gr.</p>
+                    <p><strong>Category:</strong> {product.category}</p>
+                    <p className='description'><strong>Description:</strong> {product.description}</p>
+                </div>
             </div>
-            <div className='container--product-item-details'>
-                <p><strong>Price:</strong> ${product?.price}</p>
-                <p><strong>Weight:</strong> {product?.weight} gr.</p>
-                <p><strong>Category:</strong> {product?.category}</p>
-                <p className='description'><strong>Description:</strong> {product?.description}</p>
-            </div>
-        </div>
+            ):(
+                <h3>Product not found...</h3>
+            )}
+        </>
     )
 }
 
