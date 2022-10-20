5124561e69c5fe8e7605dae3f41f1975f4b4653b
panosjapan7 <panosjapan@gmail.com>
2022-10-20 13:40:05 +0200

    Rendes Product Item details

--- frontend/src/components/Product.tsx
+++ frontend/src/components/Product.tsx
@@ -4,23 +4,28 @@ import IProductItem from "../interfaces/product-item";
 
 type HomeProps = {
     allProducts: IProductItem[]
-}
+};
 
 const Product = ({allProducts}:HomeProps) => {
   return (
-    <div className='container--all-product-items'> 
-        {allProducts.map((product, index) => (
-            <div className='container--product-item' key={index}>
-                <h3>{product.name}</h3>
-                <img className='product-image' src={product.pictures[0]} alt={product.name} />
-                <p><strong>Price:</strong> ${product.price}</p>
-                <p><strong>Manufacturer:</strong> {product.manufacturer}</p>
-                <p><strong>Weight:</strong> {product.weight} gr.</p>
-                <p><strong>Category:</strong> {product.category}</p>
-                <p className='description'><strong>Description:</strong> {product.description}</p>
+    <div className='wrapper--allProducts'>
+        
+        <h2 className='title'>All Products</h2>
 
-            </div>
-        ))}
+        <div className='container--all-product-items'> 
+            {allProducts.map((product, index) => (
+                <div className='container--product-item' key={index}>
+                    <h3>{product.name}</h3>
+                    <img className='product-image' src={product.pictures[0]} alt={product.name} />
+                    <p><strong>Price:</strong> ${product.price}</p>
+                    <p><strong>Manufacturer:</strong> {product.manufacturer}</p>
+                    <p><strong>Weight:</strong> {product.weight} gr.</p>
+                    <p><strong>Category:</strong> {product.category}</p>
+                    <p className='description'><strong>Description:</strong> {product.description}</p>
+
+                </div>
+            ))}
+        </div>
     </div>
   )
 }
