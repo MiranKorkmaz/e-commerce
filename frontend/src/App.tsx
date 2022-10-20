536aadfa8db4c6009c528f846d6bdcb7318c9344
panosjapan7 <panosjapan@gmail.com>
2022-10-20 11:00:29 +0200

    Fetch products data from backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 import './App.css';
 
+axios.defaults.baseURL = process.env.REACT_APP_SERVER_PORT || "http://localhost:3001"
+
 function App() {
+  const [allProducts, setAllProducts] = useState([]);
+  const [allProductsFromCategories, setAllProductsFromCategories] = useState([]);
+  const [allCategories, setAllCategories] = useState([]);
+
+  const fetchAllProducts = async () => {
+    const response = await axios.get("/products");
+    console.log(response);
+  };
+  /*
+  const saveAllCategoriesToArray = () => {
+    if(allProducts) {
+      allProducts.data.map(product => {
+        setAllCategories(product.category);
+      })
+    }
+  };
+  */
+
+
+  const fetchProductsFromAllCategories = async () => {
+    const response = await axios.get("/products/category/all");
+    console.log("fetchProductsFromAllCategories:" , response);
+    
+  };
+
+  useEffect(() => {
+    fetchAllProducts()
+
+  }, []);
+
   return (
     <div >
 
