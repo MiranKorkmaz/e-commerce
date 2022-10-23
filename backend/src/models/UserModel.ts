d2ca4d7ab20f7a147ec4c5030d5c824e78558fbd
panosjapan7 <panosjapan@gmail.com>
2022-10-23 09:49:03 +0200

    Comments out cart in user model

--- backend/src/models/UserModel.ts
+++ backend/src/models/UserModel.ts
@@ -28,13 +28,13 @@ const UserSchema = new Schema({
     required: true,
   },
 
-  cart: {
-    type: Object,
-    default: {
-      total: 0,
-      count: 0,
-    },
-  },
+  // cart: {
+  //   type: Object,
+  //   default: {
+  //     total: 0,
+  //     count: 0,
+  //   },
+  // },
   orders: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order" }],
 });
 
