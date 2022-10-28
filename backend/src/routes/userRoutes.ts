a5190701773e7aef68e8848dd175279f22b92fc1
zamircohen <zamir_cohen@hotmail.com>
2022-10-28 18:30:42 +0200

    Clean code and add authentication middleware to routes in backend

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -73,33 +73,47 @@ userRoutes.get("/", authMiddleware, async (req: Request, res: Response) => {
   }
 });
 
-userRoutes.get("/user/:id", authMiddleware, async (req: Request, res: Response) => {
-  const { id } = req.params;
-  try {
-    const user = await UserModel.findById(id);
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ message: "Something went wrong" });
+userRoutes.get(
+  "/user/:id",
+  authMiddleware,
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const user = await UserModel.findById(id);
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ message: "Something went wrong" });
+    }
   }
-});
+);
 
-userRoutes.put("/user/:id", async (req: Request, res: Response) => {
-  const { id } = req.params;
-  try {
-    const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(500).json({ message: "Something went wrong" });
+userRoutes.put(
+  "/user/:id",
+  authMiddleware,
+  async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const user = await UserModel.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(500).json({ message: "Something went wrong" });
+    }
   }
-});
+);
 
-userRoutes.get("/:id/orders", async (req: Request, res: Response) => {
-  try {
-    const user = await UserModel.findById(req.params.id).populate("orders");
-    res.json(user?.orders);
-  } catch (e) {
-    res.status(400).send(e);
+userRoutes.get(
+  "/:id/orders",
+  authMiddleware,
+  async (req: Request, res: Response) => {
+    try {
+      const user = await UserModel.findById(req.params.id).populate("orders");
+      res.json(user?.orders);
+    } catch (e) {
+      res.status(400).send(e);
+    }
   }
-});
+);
 
 export default userRoutes;
--- backend/src/services/auth.ts
+++ backend/src/services/auth.ts
@@ -3,16 +3,20 @@ import jwt from "jsonwebtoken";
 
 const JWT_SECRET = "0823uoiwehfFusTKLciadfsbaasd2346sdfbjaenrw";
 
-export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+export const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const authHeader = req.header("authorization");
   const token = authHeader?.split(" ")[1];
 
   if (token == null) {
     return res.sendStatus(401);
   } else {
-  jwt.verify(token, JWT_SECRET, () => {
-    req.body.user = token;
-    next();
-  })
+    jwt.verify(token, JWT_SECRET, () => {
+      req.body.user = token;
+      next();
+    });
+  }
 };
-}
