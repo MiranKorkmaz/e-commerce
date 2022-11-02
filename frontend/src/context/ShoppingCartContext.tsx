1beeb2433eb15bdaec47565698f0d7c3db50306f
Miran Korkmaz <mirankorkmaz@outlook.com>
2022-11-02 10:27:47 +0100

    Add token to cart

--- frontend/src/context/ShoppingCartContext.tsx
+++ frontend/src/context/ShoppingCartContext.tsx
@@ -6,7 +6,7 @@ import { TShoppingCartProviderProps, TCartItemArray, ICartContents } from "../in
 type ShoppingCartContext = {
     openCart: () => void
     closeCart: () => void
-    getItemQuantity: (_id: string) => number 
+    getItemQuantity: (_id: string) => number
     increaseCartQuantity: (_id: string) => void
     decreaseCartQuantity: (_id: string) => void
     removeFromCart: (_id: string) => void
@@ -19,34 +19,40 @@ const ShoppingCartContext = createContext({} as ShoppingCartContext);
 export function useShoppingCart() {
     return useContext(ShoppingCartContext);
 };
-
-// This function implements the Provider portion of the Context
 export function ShoppingCartProvider({ children }: TShoppingCartProviderProps) {
     const [isOpen, setIsOpen] = useState(false);
-    
+
     const [cartItems, setCartItems] = useState<TCartItemArray[]>([]); // this is where all of our cart information is stored
 
-    // Fetch specific user's cart data
-    let loggedInUser:string | undefined = undefined
-    loggedInUser = "6357bc05b61a410bf051a2c2"; // Has User, Has User Cart
-    // loggedInUser = "6361f5292fa2f26d4df0728a" // Has User, no User Cart
-    
-    let url = `${process.env.REACT_APP_SERVER_PORT}/cart/${loggedInUser}`
+    let loggedUserId: string | undefined = undefined;
 
-    const createNewUserCart = async (userId:string | undefined) => {   
-        const response:ICartContents = await axios.post(`${process.env.REACT_APP_SERVER_PORT}/cart/${userId}`, {userId});
+    const token = localStorage.getItem("backend3-ecom");
+    if (token) {
+        const decodeJWT = (token: string) => {
+            const base64Url = token.split(".")[1];
+            const base64 = base64Url.replace("-", "+").replace("_", "/");
+            return JSON.parse(window.atob(base64));
+        };
+        loggedUserId = decodeJWT(token).user_id;
+
+    }
+
+    let url = `${process.env.REACT_APP_SERVER_PORT}/cart/${loggedUserId}`
+
+    const createNewUserCart = async (userId: string | undefined) => {
+        const response: ICartContents = await axios.post(`${process.env.REACT_APP_SERVER_PORT}/cart/${userId}`, { userId });
         return response;
     };
-    
+
     const fetchUserCart = async () => {
         console.log("entered fetchUserCart()");
         const response = await axios.get(`${url}`);
-        if(!response.data.userCart){
-            createNewUserCart(loggedInUser);
-            
+        if (!response.data.userCart) {
+            createNewUserCart(loggedUserId);
+
         }
-        else if(loggedInUser && !response.data.userCart.cartItems) {
-            createNewUserCart(loggedInUser);
+        else if (loggedUserId && !response.data.userCart.cartItems) {
+            createNewUserCart(loggedUserId);
         }
         else {
             await setCartItems(response.data.userCart.cartItems);
@@ -66,16 +72,16 @@ export function ShoppingCartProvider({ children }: TShoppingCartProviderProps) {
     function increaseCartQuantity(_id: string) {
         setCartItems(currItems => {
             // if the item doesn't already exist in the cart (null), return an array with existing items and add 1 new item with _id, quantity.
-            if(currItems.find(item => item._id === _id) == null) { 
-                return [...currItems, {_id, quantity: 1}]
-            } 
+            if (currItems.find(item => item._id === _id) == null) {
+                return [...currItems, { _id, quantity: 1 }]
+            }
             // if the item already exists in the cart, retain the current item and its quantity and increase its quantity by 1.
             else {
                 return currItems.map(item => {
-                    if(item._id === _id){
-                        return {...item, quantity: item.quantity + 1};
+                    if (item._id === _id) {
+                        return { ...item, quantity: item.quantity + 1 };
                     }
-                    
+
                     // Return the item without any changes at all
                     else {
                         return item;
@@ -88,14 +94,14 @@ export function ShoppingCartProvider({ children }: TShoppingCartProviderProps) {
     function decreaseCartQuantity(_id: string) {
         setCartItems(currItems => {
             // if the quantity of the item that we find is 1, remove it
-            if(currItems.find(item => item._id === _id)?.quantity === 1) { 
+            if (currItems.find(item => item._id === _id)?.quantity === 1) {
                 return currItems.filter(item => item._id !== _id); // return a list of all items except the one we want to remove
-            } 
+            }
             // if the item already exists in the cart, retain the current item and its quantity and decrease its quantity by 1.
             else {
                 return currItems.map(item => {
-                    if(item._id === _id){
-                        return {...item, quantity: item.quantity - 1};
+                    if (item._id === _id) {
+                        return { ...item, quantity: item.quantity - 1 };
                     }
                     // Return the item without any changes at all
                     else {
@@ -113,24 +119,24 @@ export function ShoppingCartProvider({ children }: TShoppingCartProviderProps) {
     };
 
     useEffect(() => {
-        if(loggedInUser) {
+        if (loggedUserId) {
             fetchUserCart();
         }
-    }, []);
+    }, [token]);
 
     return (
         <ShoppingCartContext.Provider value={{
-            getItemQuantity, 
-            increaseCartQuantity, 
-            decreaseCartQuantity, 
+            getItemQuantity,
+            increaseCartQuantity,
+            decreaseCartQuantity,
             removeFromCart,
             openCart,
             closeCart,
             cartItems,
             cartQuantity
-            }}>
+        }}>
             {children}
-            <ShoppingCart isOpen={isOpen}/>
+            <ShoppingCart isOpen={isOpen} />
         </ShoppingCartContext.Provider>
-    ) 
+    )
 };
